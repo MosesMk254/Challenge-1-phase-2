a7fc63345a36c08663ab19c754b9ff57a7488b33
Moses Mutisya <mutisyamoses722@gmail.com>
2024-04-27 15:38:50 +0300

    adding the transction table and form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TransactionTable from "./components/TransactionTable";
+import TransactionForm from "./components/TransactionForm";
+
+const TransactionsList = [
+  {
+    date: "2019-12-01",
+    description: "Paycheck from Bob's Burgers",
+    category: "Income",
+    amount: 1000,
+  },
+  {
+    date: "2019-12-01",
+    description: "South by Southwest Quinoa Bowl at Fresh & Co",
+    category: "Food",
+    amount: -10.55,
+  },
+  {
+    date: "2019-12-02",
+    description: "South by Southwest Quinoa Bowl at Fresh & Co",
+    category: "Food",
+    amount: -10.55,
+  },
+  {
+    date: "2019-12-04",
+    description: "Sunglasses, Urban Outfitters",
+    category: "Fashion",
+    amount: -24.99,
+  },
+  {
+    date: "2019-12-06",
+    description: "Venmo, Alice Pays you for Burrito",
+    category: "Food",
+    amount: 8.75,
+  },
+  {
+    date: "2019-12-06",
+    description: "Chipotle",
+    category: "Food",
+    amount: -17.59,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TransactionForm />
+      <TransactionTable transactions={TransactionsList} />
     </div>
   );
 }
