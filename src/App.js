a083190026ce529be67b50929d36f21a38c2b4b1
Moses Mutisya <mutisyamoses722@gmail.com>
2024-04-27 21:01:21 +0300

    displaying trasation form details to the transaction table

--- src/App.js
+++ src/App.js
@@ -1,56 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import TransactionTable from "./components/TransactionTable";
 import TransactionForm from "./components/TransactionForm";
 import SearchOption from "./components/Search";
 import DisplayHeader from "./components/Header";
 
-const TransactionsList = [
-  {
-    date: "2019-12-01",
-    description: "Paycheck from Bob's Burgers",
-    category: "Income",
-    amount: 1000,
-  },
-  {
-    date: "2019-12-01",
-    description: "South by Southwest Quinoa Bowl at Fresh & Co",
-    category: "Food",
-    amount: -10.55,
-  },
-  {
-    date: "2019-12-02",
-    description: "South by Southwest Quinoa Bowl at Fresh & Co",
-    category: "Food",
-    amount: -10.55,
-  },
-  {
-    date: "2019-12-04",
-    description: "Sunglasses, Urban Outfitters",
-    category: "Fashion",
-    amount: -24.99,
-  },
-  {
-    date: "2019-12-06",
-    description: "Venmo, Alice Pays you for Burrito",
-    category: "Food",
-    amount: 8.75,
-  },
-  {
-    date: "2019-12-06",
-    description: "Chipotle",
-    category: "Food",
-    amount: -17.59,
-  },
-];
-
 function App() {
+  const [transactions, setTransaction] = useState([
+    {
+      date: "2019-12-01",
+      description: "Paycheck from Bob's Burgers",
+      category: "Income",
+      amount: 1000,
+    },
+    {
+      date: "2019-12-01",
+      description: "South by Southwest Quinoa Bowl at Fresh & Co",
+      category: "Food",
+      amount: -10.55,
+    },
+    {
+      date: "2019-12-02",
+      description: "South by Southwest Quinoa Bowl at Fresh & Co",
+      category: "Food",
+      amount: -10.55,
+    },
+    {
+      date: "2019-12-04",
+      description: "Sunglasses, Urban Outfitters",
+      category: "Fashion",
+      amount: -24.99,
+    },
+    {
+      date: "2019-12-06",
+      description: "Venmo, Alice Pays you for Burrito",
+      category: "Food",
+      amount: 8.75,
+    },
+    {
+      date: "2019-12-06",
+      description: "Chipotle",
+      category: "Food",
+      amount: -17.59,
+    },
+  ]);
+
+  const addTransaction = (newTransaction) => {
+    setTransaction([...transactions, newTransaction]);
+  };
   return (
     <div>
       <DisplayHeader />
       <SearchOption />
-      <TransactionForm />
-      <TransactionTable transactions={TransactionsList} />
+      <TransactionForm onAddTrasaction={addTransaction} />
+      <TransactionTable transactions={transactions} />
     </div>
   );
 }
--- src/components/TransactionForm.js
+++ src/components/TransactionForm.js
@@ -1,17 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
-const TransactionForm = () => {
+const TransactionForm = ({ onAddTrasaction }) => {
+  const [date, setDate] = useState("");
+  const [description, setDescription] = useState("");
+  const [amount, SetAmount] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTransaction = {
+      date,
+      description,
+      category,
+      amount: parseFloat(amount),
+    };
+    onAddTrasaction(newTransaction);
+    setDate("");
+    setDescription("");
+    setCategory("");
+    SetAmount("");
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Date</label>
-        <input type="date" />
+        <input
+          type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
 
-        <input type="text" placeholder="Description" />
+        <input
+          type="text"
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
 
-        <input type="text" placeholder="Category" />
+        <input
+          type="text"
+          placeholder="Category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        />
 
-        <input type="number" placeholder="Amount" />
+        <input
+          type="number"
+          placeholder="Amount"
+          value={amount}
+          onChange={(e) => SetAmount(e.target.value)}
+        />
 
         <button type="submit">Add Transaction</button>
       </form>
